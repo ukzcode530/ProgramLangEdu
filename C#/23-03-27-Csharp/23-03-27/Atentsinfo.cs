2d16e7c6f8405e20836b6c12140dc2f943252722
ukzcode530 <goodi530@naver.com>
2023-03-27 18:20:48 +0900

    [23-03-27] 오버로딩 및 ref 참조   네임스페이스 필드 개념

---
+++ C#/23-03-27-Csharp/23-03-27/Atentsinfo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtentsJungBo
+{
+    class Atentsinfo
+    {
+        public void Swap(ref int a, ref int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+
+        }
+
+        public void CalculMax(int a, int b, int c)
+        {
+            int maxNum;
+
+            if (a > b && a > c)
+            {
+                maxNum = a;
+            }
+            else if (b > c)
+            {
+                maxNum = b;
+            }
+            else
+            {
+                maxNum = c;
+            }
+
+            Console.WriteLine("Max {0}", maxNum);
+        }
+
+        public void CalculMin(int a, int b, int c)
+        {
+            int minNum;
+
+            if (a < b && a < c)
+            {
+                minNum = a;
+            }
+            else if (b < c)
+            {
+                minNum = b;
+            }
+            else
+            {
+                minNum = c;
+            }
+
+            Console.WriteLine("Min  {0} ", minNum);
+        }
+
+        public static void Divide(int a, int b, out int quotient, out int remainder)
+        {
+            quotient = a / b;
+            remainder = a % b;
+
+        }
+
+        public static void PrintProfile(string name, string phone)
+        {
+            Console.WriteLine("Name : {0} ,Phone{1}", name, phone);
+
+        }
+
+       public void MyMethod(int a = 0, int b = 0) // 선택 매개변수
+        {
+            Console.WriteLine("{0} {1}", a, b);
+        }
+
+
+    }
+}
---
+++ C#/23-03-27-Csharp/23-03-27/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AtentsJungBo;
+
+
+namespace _23_03_27
+{
+    class MainApp
+    {
+
+        //static double Square(double arg)
+        //{
+        //    // 이 메소드를 구현해주세요.
+        //    double result;
+
+        //    result = arg * arg;
+
+        //    return result;
+        //}
+
+        static void Main(string[] args)
+        {
+
+            int a = 3;
+            int b = 4;
+            int resultA = 0;
+
+
+            Plus(a, b, out resultA);
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, resultA);
+
+            double x = 2.4;
+            double y = 3.1;
+            double resultB = 0;
+
+
+            Plus(x, y, out resultB);
+
+
+            Console.WriteLine("{0} + {1} = {2}", x, y, resultB);
+
+
+
+            //double mean = 0;
+
+            //Mean(1, 2, 3,  4,  5, ref mean);
+
+            //Console.WriteLine("평균 : {0}",mean);
+
+            //while(true)
+            //    { 
+            //    Console.Write("수를 입력하세요:");
+            //    string input = Console.ReadLine();
+            //    double arg = Convert.ToDouble(input);
+
+            //    Console.WriteLine("결과:{0}",Square(arg));
+            //    }
+
+
+
+            //Atentsinfo atents = new Atentsinfo();
+            //int x = 20;
+            //int y = 30;
+            //atents.Swap(ref x,ref y);
+            //Console.WriteLine("x{0} y{1}",x,y);
+            //int num1;
+            //int num2;
+            //int num3;
+
+            //Console.WriteLine("정수 세개 입력:");
+
+            //Console.Write("첫번째: ");
+            //num1 = int.Parse(Console.ReadLine());
+
+            //Console.Write("두번째: ");
+            //num2 = int.Parse(Console.ReadLine());
+
+            //Console.Write("세번째: ");
+            //num3 = int.Parse(Console.ReadLine());
+
+
+            //atents.CalculMax(num1, num2, num3);
+            //atents.CalculMin(num1, num2, num3);
+            ////출력 전용 매개 변수
+
+            //int a = 20;
+            //int b = 3;
+            //int c, d;
+            //Atentsinfo.Divide(a, b, out c, out d);
+            //Console.WriteLine();
+
+
+            //Atentsinfo atentsInfo = new Atentsinfo();
+
+
+
+
+        }
+
+        public static void Plus(int a, int b, out int c)
+        {
+
+            c = a + b;
+
+        }
+
+        public static void Plus(double a, double b, out double c)
+        {
+
+            c = a + b;
+
+        }
+        //public static void Mean(
+
+        //    double a, double b, double c,
+        //    double d, double e, ref double mean)
+        //{
+
+        //    mean = (a + b + c + d +  e) / 5;
+
+        //}
+
+        //public static void Divide(int a, int b, out int quotient,out int refmainer)
+        //{
+        //    quotient = a / b;
+        //    refmainer = a % b;
+        //}
+
+    }
+    }
